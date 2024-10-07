fbb8fae7cc90aff595a9eeced52f56b32d3b5c35
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-10-07 16:22:02 -0600

    setting up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import Profile from './pages/Profile'
+import Register from './pages/Register'
+import NavBar from './components/Layout/NavBar'
+import Footer from './components/Layout/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <NavBar />
+            <Home />
+            <Footer />
+          </>
+        } />
+        <Route path="/dashboard" element={
+          <>
+            <NavBar />
+            <Dashboard />
+            <Footer />
+          </>
+        } />
+        <Route path="/profile" element={
+          <>
+            <NavBar />
+            <Profile />
+            <Footer />
+          </>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
