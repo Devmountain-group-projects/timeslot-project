237e306cef0f356fa8194f4672d018e0fc751160
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-10-07 16:04:23 -0600

    pushing up scripts and database setup. fixed a typo in a file name

---
+++ backend/config/db.js
@@ -0,0 +1,40 @@
+import { Sequelize } from "sequelize";
+// environment setup for db. Sets the db name and other configs
+import config from "/config.json" with { type: "json" };
+
+const env = "development";
+const dbConfig = config[env];
+
+// Initializing an empty object to store db models
+const db = {};
+
+// Create a new Sequelize instance, passing in the database name, username, and password
+const sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig,
+);
+
+// Importing models
+// import { User } from "./user.js";
+
+// Associating models for use in the db
+// const user = User(sequelize, Sequelize.DataTypes);
+
+// Storing the models in the db object
+// db[role.name] = role;
+
+// Setting up relationships between models
+Object.keys(db).forEach((modelName) => {
+    // check if the model has an associate method
+    if (db[modelName].associate) {
+        //   // if the model has an associate method, call it
+        db[modelName].associate(db);
+    }
+});
+
+// Exporting the db object
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+export default db;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,57 @@
+// Imports
 import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
 import morgan from 'morgan';
-import connectDB from './config/db.js';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import ViteExpress from 'vite-express';
 
+// Express Setup
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    }),
+)
+app.use(
+    session({
+        secret: 'keyboard warrior',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+// Database and seed data
+import db from './models/db.js';
+
+db.sequelize.sync({ force: true }).then(async function () {
+    console.log("Database successfully created");
+// Sample db data
+    // try {
+    //     // create temp data for testing, remove after testing is done
+    //     await createUser(db).then(() =>
+    //         console.log("Permissions successfully created"),
+    //     );
+    //
+    // } catch (err) {
+    //     console.error("Error during seeding:", err);
+    // }
+
+    app.set("db", db);
+
+    ViteExpress.config({ printViteDevServerHost: true });
+    ViteExpress.listen(app, 5539, () =>
+        console.log("Server running on http://localhost:5539"),
+    );
+});
+
+// Routes
+// import appointment from './routes/appointmentRoutes.js';
+// import auth from './routes/authRoutes.js';
+// import business from './routes/businessRoutes.js';
+// import contact from './routes/contactRoutes.js';
