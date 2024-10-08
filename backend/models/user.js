1c4c9fa4fd0d9e6b35701f5e39813c7b1230f1da
Jareth Carpenter <Jarethcarp@gmail.com>
2024-10-08 10:10:37 -0600

    Updated the dataa model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,74 +1,10 @@
-// import { Model, DataTypes } from "sequelize";
-
-// class User extends Model {
-//   static init(sequelize) {
-//     return super.init(
-//       {
-//         id: {
-//           type: DataTypes.UUID,
-//           defaultValue: DataTypes.UUIDV4,
-//           primaryKey: true,
-//         },
-//         name: {
-//           type: DataTypes.STRING,
-//           allowNull: false,
-//         },
-//         email: {
-//           type: DataTypes.STRING,
-//           allowNull: false,
-//           unique: true,
-//         },
-//         phone: {
-//           type: DataTypes.STRING,
-//           allowNull: false,
-//         },
-//         role: {
-//           type: DataTypes.STRING,
-//           defaultValue: "client",
-//           allowNull: false,
-//         },
-//         password_hash: {
-//           type: DataTypes.STRING,
-//           allowNull: false,
-//         },
-//         profile_picture: {
-//           type: DataTypes.STRING,
-//           allowNull: false,
-//         },
-//         created_at: {
-//           type: DataTypes.TIMESTAMP,
-//           defaultValue: CURRENT_TIMESTAMP(),
-//           allowNull: false,
-//         },
-//         updated_at: {
-//           type: DataTypes.TIMESTAMP,
-//           defaultValue: CURRENT_TIMESTAMP(),
-//           allowNull: false,
-//         },
-//       },
-//       {
-//         sequelize,
-//         modelName: "Users",
-//       }
-//     );
-//   }
-
-//   static associate(model) {
-//     this.hasMany(model.Appointment)
-//     this.hasMany(model.Business)
-//   }
-// }
-
-// export default User
-
-
 export const User = (sequelize, Sequelize) => {
   const user = sequelize.define(
     "user", {
       
       id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
         primaryKey: true,
       },
       name: {
@@ -97,16 +33,6 @@ export const User = (sequelize, Sequelize) => {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // created_at: {
-      //   type: Sequelize.DATE,
-      //   defaultValue: Sequelize.DATE("NOW"),
-      //   allowNull: false,
-      // },
-      // updated_at: {
-      //   type: Sequelize.DATE,
-      //   defaultValue: Sequelize.DATE("NOW"),
-      //   allowNull: false,
-      // },
     },
     {
       timestamps: true,
