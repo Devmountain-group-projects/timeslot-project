8436c5189645fe67475611536d47d779d1d71645
Jareth Carpenter <Jarethcarp@gmail.com>
2024-10-08 16:34:23 -0600

    Set up foreignKeys

--- backend/models/business.js
+++ backend/models/business.js
@@ -1,44 +1,70 @@
 export const Business = (sequelize, Sequelize) => {
-    const business = sequelize.define(
-        "business", {
-            business_id: {
-                type: Sequelize.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-            },
-            user_id: {
-                type: Sequelize.INTEGER,
-            },
-            business_name: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            description: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            address: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            email: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            phone: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            website: {
-                type: Sequelize.STRING,
-                allowNull: false
-            }
-        }, 
-        {
-            timestamps: true,
-        }
-    )
-    // Relations
+  const business = sequelize.define(
+    "business",
+    {
+      business_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      business_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      website: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  // Relations
+  business.associate = (models) => {
+    business.belongsToMany(models.user, {
+      foreignKey: "user_id",
+      onDelete: "RESTRICT",
+      through: "user"
+    });
 
-    return business
-}
+    business.belongsToMany(models.service, {
+      foreignKey: "business_id",
+      onDelete: "RESTRICT",
+      through: "service"
+    });
+
+    business.belongsToMany(models.availability, {
+      foreignKey: "business_id",
+      onDelete: "RESTRICT",
+      through: "availability"
+    });
+
+    business.belongsToMany(models.appointment, {
+        foreignKey: 'business_id',
+        onDelete: "RESTRICT",
+      through: "appointment"
+    })
+  };
+
+  return business;
+};
--- backend/models/notification.js
+++ backend/models/notification.js
@@ -1,40 +1,52 @@
 export const Notification = (sequelize, Sequelize) => {
-    const notification = sequelize.define(
-        "notification", {
-            notification_id: {
-                type: Sequelize.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-            },
-            user_id: {
-                type: Sequelize.INTEGER,
-            },
-            appointment_id: {
-                type: Sequelize.INTEGER,
-            },
-            message: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            type: {
-                type: Sequelize.ENUM('email', 'SMS', 'in-app', 'test'),
-                allowNull: false
-            },
-            sent_at: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            status: {
-                type: Sequelize.ENUM('sent', 'pending', 'failed', 'unread', 'test'),
-                defaultValue: 'pending',
-                allowNull: false
-            }
-        }, 
-        {
-            timestamps: true,
-        }
-    )
-    // Relations
+  const notification = sequelize.define(
+    "notification",
+    {
+      notification_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      appointment_id: {
+        type: Sequelize.INTEGER,
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM("email", "SMS", "in-app", "test"),
+        allowNull: false,
+      },
+      sent_at: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM("sent", "pending", "failed", "unread", "test"),
+        defaultValue: "pending",
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  // Relations
+  notification.associate = (models) => {
+    notification.belongsTo(models.appointment, {
+      foreignKey: "appointment_id",
+      onDelete: "RESTRICT",
+    });
 
-    return notification
-}
+    notification.belongsTo(models.user, {
+      foreignKey: "user_id",
+      onDelete: "RESTRICT",
+    });
+  };
+
+  return notification;
+};
