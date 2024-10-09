ba3402b538fdbbdcd7a7f3fcbed0e998508d3c94
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-10-09 16:42:04 -0600

    had to make seperate junction tables for business and user

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,61 +1,67 @@
 export const User = (sequelize, Sequelize) => {
-  const user = sequelize.define(
-    "user",
-    {
-      user_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      phone: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      role: {
-        type: Sequelize.STRING,
-        defaultValue: "client",
-        allowNull: false,
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      profile_picture: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      timestamps: true,
-    }
-  );
-  // Relations
-  user.associate = (models) => {
-    user.belongsToMany(models.business, {
-      foreignKey: "user_id",
-      onDelete: "RESTRICT",
-      through: "business"
-    });
+    const user = sequelize.define(
+        "user",
+        {
+            user_id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            phone: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            role: {
+                type: Sequelize.STRING,
+                defaultValue: "client",
+                allowNull: false,
+            },
+            password_hash: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            profile_picture: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            timestamps: true,
+        }
+    );
+    // Relations
+    user.associate = (models) => {
+        user.belongsToMany(models.business, {
+            foreignKey: "user_id",
+            onDelete: "RESTRICT",
+            through: "business"
+        });
 
-    user.hasMany(models.notification, {
-      foreignKey: "notification_id",
-      onDelete: "RESTRICT",
-    });
+        user.hasMany(models.notification, {
+            foreignKey: "notification_id",
+            onDelete: "RESTRICT",
+        });
 
-    user.hasMany(models.appointment, {
-      foreignKey: "appointment_id",
-      onDelete: "RESTRICT",
-    });
-  };
+        user.hasMany(models.appointment, {
+            foreignKey: "appointment_id",
+            onDelete: "RESTRICT",
+        });
 
-  return user;
+        user.belongsToMany(models.image, {
+            foreignKey: "user_id",
+            onDelete: "RESTRICT",
+            through: "image_user",
+        });
+    };
+
+    return user;
 };
--- backend/seeds/userSeed.js
+++ backend/seeds/userSeed.js
@@ -8,6 +8,16 @@ const users = [
         role: "test",
         password_hash: "test",
         profile_picture: "test",
+        photos: [
+            {
+                src: "https://photos.zillowstatic.com/fp/f180a30f2bdd6ffd689d1d7f85edcf81-cc_ft_1536.webp",
+                is_primary: true,
+            },
+            {
+                src: "https://photos.zillowstatic.com/fp/6c91e814756194bd0e18bfe37a552c61-cc_ft_768.webp",
+                is_primary: false,
+            },
+        ],
     },
 ];
 
@@ -18,15 +28,35 @@ export const createUsers = async function createUsers(db) {
             bcryptjs.genSaltSync(10),
         );
 
+        const profile_photos = user.photos;
+
+        let photos = profile_photos.map((photo) => {
+            return {
+                src: photo.src,
+                property_image: {
+                    is_primary: photo.is_primary,
+                },
+            };
+        });
+
         await db.user
             .create({
-                name: user.name.toLowerCase(),
-                email: user.email.toLowerCase(),
-                phone: user.phone,
-                role: user.role,
-                password_hash: hashedPassword,
-                profile_picture: user.profile_picture,
-            }
-        );
+                    name: user.name.toLowerCase(),
+                    email: user.email.toLowerCase(),
+                    phone: user.phone,
+                    role: user.role,
+                    password_hash: hashedPassword,
+                    profile_picture: user.profile_picture,
+                    images: photos,
+                },
+                {
+                    include: [
+                        {
+                            model: db.sequelize.models.image,
+                            as: "images",
+                        },
+                    ],
+                }
+            );
     }
 };
