7bf741361790df8f0cb8cdf0dc6e7154092c06db
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-10-11 09:32:16 -0600

    installed react redux

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,26 +1,26 @@
-const inittalState = {
+const initialState = {
   userId: null,
   loggedIn: false,
 };
 
-const userReducer = (state = inittalState, action) => {
+const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "USER_LOGOUT":
-      console.log("Reducer has logged out the user");
+    case 'USER_LOGOUT':
+      console.log('Reducer has logged out the user');
       return {
         ...state,
         loggedIn: false,
       };
 
-    case "USER_LOGIN":
-      console.log("Reducer has logged in the user");
+    case 'USER_LOGIN':
+      console.log('Reducer has logged in the user');
       return {
         ...state,
         loggedIn: true,
       };
 
     default:
-      console.log("DEFAULT");
+      console.log('DEFAULT');
       return state;
   }
 };
