5f2eeeaa6e499dabb10fb06b6bd163190d990a6e
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-10-14 14:54:38 -0600

    made adjustments to component font sizes and other styling to fit better in smaller resolution screens

--- src/components/BusinessDashboard/ClientReviews.jsx
+++ src/components/BusinessDashboard/ClientReviews.jsx
@@ -8,20 +8,20 @@ const ClientReviews = () => {
         <div className="bg-white w-full h-full flex flex-col rounded-xl border-2 border-gray-300">
             <section className="h-[35%] flex justify-between items-center p-4 bg-tertiary">
                 <div className="w-[10%]">
-                    <RiUserStarFill className="text-3xl text-[#cd942d]" />
+                    <RiUserStarFill className="text-xl md:text-2xl text-[#cd942d]" />
                 </div>
-                <h2 className="w-[80%] text-base text-center font-medium">Client Reviews</h2>
+                <h2 className="w-[80%] text-xs md:text-sm text-center font-medium">Client Reviews</h2>
                 <div className="w-[10%] flex justify-end">
                     <FaChevronRight className="text-gray-400" />
                 </div>
             </section>
-            <hr className='border-gray-300 border-2 w-full' />
+            <hr className='border-gray-300 border-1 w-full' />
             <section className="h-[65%] flex justify-between items-center p-4">
                 <div className="w-[70%]">
-                    <p className="text-3xl font-bold">12</p>
+                    <p className="text-xl md:text-2xl font-medium">12</p>
                     <p className="text-sm text-gray-500">New reviews</p>
                 </div>
-                <img src={Graph1} className="w-[30%]" alt="Graph image icon" />
+                <img src={Graph1} className="w-[20%] md:w-[30%]" alt="Graph image icon" />
             </section>
         </div>
     );
--- src/components/BusinessDashboard/Notifications.jsx
+++ src/components/BusinessDashboard/Notifications.jsx
@@ -8,20 +8,20 @@ const Notifications = () => {
         <div className="bg-white w-full h-full flex flex-col rounded-xl border-2 border-gray-300">
             <section className="h-[35%] flex justify-between items-center p-4 bg-tertiary">
                 <div className="w-[10%]">
-                    <RiNotificationBadgeFill className="text-3xl text-[#3cbd5b]" />
+                    <RiNotificationBadgeFill className="text-xl md:text-2xl text-[#3cbd5b]" />
                 </div>
-                <h2 className="w-[80%] text-base text-center font-medium">Notifications</h2>
+                <h2 className="w-[80%] text-xs md:text-sm text-center font-medium">Notifications</h2>
                 <div className="w-[10%] flex justify-end">
                     <FaChevronRight className="text-gray-400" />
                 </div>
             </section>
-            <hr className='border-gray-300 border-2 w-full' />
+            <hr className='border-gray-300 border-1 w-full' />
             <section className="h-[65%] flex justify-between items-center p-4">
                 <div className="w-[70%]">
-                    <p className="text-3xl font-bold">0</p>
-                    <p className="text-sm text-gray-500">New notifications</p>
+                    <p className="text-xl md:text-2xl font-medium">0</p>
+                    <p className="text-xs md:text-sm text-gray-500">New notifications</p>
                 </div>
-                <img src={Graph1} className="w-[30%]" alt="Graph image icon" />
+                <img src={Graph1} className="w-[20%] md:w-[30%]" alt="Graph image icon" />
             </section>
         </div>
     );
