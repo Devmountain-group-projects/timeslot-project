5f0a718fef2a1ff771e2858f84f755e92588e895
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-10-15 12:05:36 -0600

    added review table and fixed a realtion on user and conversation

--- backend/models/review.js
+++ backend/models/review.js
@@ -1,1 +1,39 @@
-// Feature to add later
+export const Review = (sequelize, Sequelize) => {
+    const review = sequelize.define(
+        "review",
+        {
+            review_id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'user',
+                    key: 'user_id',
+                },
+                onDelete: "RESTRICT",
+            },
+            business_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'business',
+                    key: 'business_id',
+                },
+                onDelete: "RESTRICT",
+            },
+            rating: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            comment: {
+                type: Sequelize.TEXT,
+            },
+        },
+        {
+            timestamps: true,
+        }
+    );
+    return review;
+}
