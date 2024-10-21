5df3a55605cd259692e3b148a344c9add00d0dce
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-10-21 16:58:58 -0600

    AllReviews is responsive and cards too

--- src/components/BusinessDashboard/AllReviews.jsx
+++ src/components/BusinessDashboard/AllReviews.jsx
@@ -31,44 +31,49 @@ const AllReviews = () => {
 
     return (
         <motion.div
-            className="h-full flex gap-4 overflow-hidden"
+            className="h-full flex flex-col gap-4 overflow-auto relative"
             variants={containerVariants}
             initial="hidden"
             animate="visible"
         >
-            {/* Column 1 - 60% width */}
-            <div className="w-[60%] flex flex-col gap-4">
-                {/* Top row - 30% height */}
-                <motion.div className="h-[30%] flex gap-4" variants={rowVariants}>
-                    <Card className="w-1/3"><Overview /></Card>
-                    <Card className="w-2/3"><FeaturedReviews /></Card>
-                </motion.div>
-                {/* Bottom row - 70% height */}
-                <motion.div className="h-[70%]" variants={rowVariants}>
-                    <Card className="h-full overflow-hidden">
+            {/* Column 1 */}
+            <motion.div className="flex-shrink-0 flex flex-col lg:flex-row gap-4 lg:h-full" variants={rowVariants}>
+                {/* Left section - Overview and Featured Reviews */}
+                <div className="w-full lg:w-[60%] flex flex-col gap-4 lg:h-full">
+                    {/* Top row with Overview and Featured Reviews */}
+                    <div className="flex flex-col sm:flex-row gap-4 lg:h-[30%]">
+                        <Card className="w-full sm:w-1/3 h-[280px] lg:h-full">
+                            <Overview />
+                        </Card>
+                        <Card className="w-full sm:w-2/3 h-[250px] lg:h-full">
+                            <FeaturedReviews />
+                        </Card>
+                    </div>
+                    {/* Review Management section */}
+                    <Card className="flex-grow h-auto lg:h-[70%]">
                         <ReviewMgmt />
                     </Card>
-                </motion.div>
-            </div>
+                </div>
 
-            {/* Column 2 - 40% width */}
-            <div className="w-[40%] flex flex-col gap-4">
-                <motion.div className="h-1/3" variants={rowVariants}>
-                    <Card className="h-full overflow-hidden"><ReviewHistoryTrend /></Card>
-                </motion.div>
-                <motion.div className="h-1/3" variants={rowVariants}>
-                    <Card className="h-full overflow-hidden"><ClientSentiment /></Card>
-                </motion.div>
-                <motion.div className="h-1/3" variants={rowVariants}>
-                    <Card className="h-full overflow-hidden"><ReviewsByService /></Card>
-                </motion.div>
-            </div>
+                {/* Right section - Trends and Stats */}
+                <div className="w-full lg:w-[40%] flex flex-col gap-4 lg:h-full">
+                    <Card className="h-[350px] lg:h-1/3">
+                        <ReviewHistoryTrend />
+                    </Card>
+                    <Card className="h-[350px] lg:h-1/3">
+                        <ClientSentiment />
+                    </Card>
+                    <Card className="h-[350px] lg:h-1/3">
+                        <ReviewsByService />
+                    </Card>
+                </div>
+            </motion.div>
         </motion.div>
     )
 }
 
 const Card = ({ children, className = '' }) => (
-    <div className={`bg-white rounded-xl shadow-sm ${className}`}>
+    <div className={`bg-white rounded-xl shadow-sm overflow-auto ${className}`}>
         {children}
     </div>
 )
